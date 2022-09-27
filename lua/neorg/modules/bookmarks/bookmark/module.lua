5059a29ffccffed3da34f1daad2040177fdaa081
Simon H Moore <simon@simonhugh.xyz>
2022-09-27 22:48:08 +0100

    add: return neorg module

--- lua/neorg/modules/bookmarks/bookmark/module.lua
+++ lua/neorg/modules/bookmarks/bookmark/module.lua
@@ -3,3 +3,8 @@
     A Neorg module designed to create, edit, search and organise bookmarks
 --]]
 
+require('neorg.modules.base')
+
+local module = neorg.modules.create("bookmarks.bookmark")
+
+return module
